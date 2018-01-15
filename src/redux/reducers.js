7b1f658d24c1566cf9ccf5e91c09a6ba39f61d64
Chul hee Lee <chulheelee@Chului-MacBook-Pro.local>
2018-01-15 12:50:39 +0900

    working...

---
+++ src/redux/reducers.js
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux';
+import { ACTION_LOGIN, ACTION_LOGOUT } from './actions';
+
+function login(state, action) {
+  switch (action) {
+    case ACTION_LOGIN:
+      // login api
+      break;
+    case ACTION_LOGOUT:
+      // logout api
+      break;
+    default:
+      break;
+  }
+}
+
+function create() {
+
+}
