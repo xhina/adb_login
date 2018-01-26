04a418f47b3867762ef4a8f0b15a3816ca1e6500
Chul hee Lee <chulheelee@Chului-iMac.local>
2018-01-26 21:43:56 +0900

    api working / add loading indicator (2)

---
+++ client/src/js/view/loading_indicator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ImageRes} from '../res-link';
+
+class view extends React.Component {
+
+  render() {
+    return (
+      <div id="indicator" style={{display:"none"}}>
+        <img src={ImageRes.loding_indicator} alt="" />
+      </div>
+    );
+  }
+}
+
+export default view;
